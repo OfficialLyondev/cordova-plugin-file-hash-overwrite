a716e6e9e64019f3b9b5c1263b2372a2be0fd248
OfficialLyondev <91533176+OfficialLyondev@users.noreply.github.com>
2022-02-03 01:53:42 +0100

    Update FileHash.java

--- src/android/FileHash.java
+++ src/android/FileHash.java
@@ -119,7 +119,8 @@ public class FileHash extends CordovaPlugin {
 			for (int i = 0; i < mdbytes.length; i++) { sb.append(Integer.toString((mdbytes[i] & 0xff) + 0x100, 16).substring(1)); }
 			ret[0] = sb.toString();
 			System.out.println(sb.toString());
-			return "test";
+			ret[0] = "test";
+			return ret;
 			}
 		catch(Exception ex){
 			if(ex instanceof FileNotFoundException){
